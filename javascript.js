210cf42ecc687069d369af5ad415be7a04927d0a
sunslifer <87036328+sunslifer@users.noreply.github.com>
2021-07-28 19:24:13 -0700

    Add Files
    First update

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function validate(){
+    event.preventDefault();
+
+	if(document.myForm.Name.value == '')
+	{
+		document.myForm.Email.focus();
+		var Email = document.getElementById("Email");
+        console.log(Email);
+		Email.style.backgroundColor = "aqua";
+		Email.style.border = 'thick solid red';
+		var label = document.createElement('label');
+		label.textContent = "Please provide a valid Email";
+		var div = document.getElementById("showmessage");
+		div.appendChild(label)
+
+		return false;
+
+	} else {
+        var youname = document.getElementById('Email').value;
+        console.log(Email);
+	}
+}
